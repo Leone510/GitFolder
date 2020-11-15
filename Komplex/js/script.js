43bfe121a5c78a93a25bab6d3cf043c649dd6f30
Leone510 <meteorologsio@gmail.com>
2020-11-15 10:31:28 +0200

    Komplex Work

---
+++ Komplex/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+   $('.slider').slick({
+      arrows:false,
+      infinite: true,
+      slidesToShow: 1,
+      speed: 1000,
+      autoplay:true,
+      autoplaySpeed: 3000,
+
+   });
+ });
