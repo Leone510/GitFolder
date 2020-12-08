36251e86f38a82dfdf4280d3a3cad21bab700244
Leone510 <meteorologsio@gmail.com>
2020-12-08 21:25:03 +0200

    Task2

---
+++ JavaScript/HW3/task 2/js/script.js
@@ -0,0 +1,19 @@
+function Emploee (id, name, surname, salary, experience, privileges, gender) {
+   this.id = id;
+   this.name = name;
+   this.surname = surname;
+   this.salary = salary;
+   this.workExperience = experience;
+   this.isPrivileges =privileges;
+   this.gender = gender;
+}
+
+Emploee.prototype.getFullName = function () {
+   return this.surname + ' ' + this.name;
+}
+
+const employeeObj = new Emploee (0, 'Valeriy', 'Zhmishenko', 1000, 10, true, 'male');
+
+console.log(employeeObj.getFullName());
+
+
