6c6d558c9cf4cdbe2484be925e87e8d61a055adb
Leone510 <meteorologsio@gmail.com>
2020-12-09 19:55:12 +0200

    Task3

---
+++ JavaScript/HW3/task 3/js/script.js
@@ -0,0 +1,30 @@
+function Emploee (id, name, surname, salary, experience, privileges, gender) {
+   this.id = id;
+   this.name = name;
+   this.surname = surname;
+   this.salary = salary;
+   this.workExperience = experience;
+   this.isPrivileges = privileges;
+   this.gender = gender;
+}
+
+Emploee.prototype.getFullName = function () {
+   return this.surname + ' ' + this.name;
+}
+
+let employeeObj = new Emploee (0, 'Valeriy', 'Zhmishenko', 1000, 10, true, 'male');
+
+let createEmployesFromArr = (arr) => {
+   let newArr = [];
+   for (let args of arr) {
+      employeeObj = new Emploee (args.id, args.name, args.surname, args.salary, args.workExperience, args.isPrivileges, args.gender)
+      newArr.push(employeeObj);
+   }
+   return newArr;
+};
+const emplyeeConstructArr = createEmployesFromArr(emplyeeArr); 
+
+console.log(emplyeeConstructArr);
+
+
+
