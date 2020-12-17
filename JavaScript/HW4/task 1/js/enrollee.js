0cb75dd4197df9c4ee9189ff5025ad372b393427
Leone510 <meteorologsio@gmail.com>
2020-12-17 07:05:47 +0200

    Task4.1

---
+++ JavaScript/HW4/task 1/js/enrollee.js
@@ -0,0 +1,68 @@
+const enrollee = [
+   {
+      name: 'Vasya',
+      surname: 'Pyatochkin',
+      ratingPoint: 800,
+      schoolPoint: 100,
+   },
+   {
+      name: 'Petia',
+      surname: 'Kurochkin',
+      ratingPoint: 820,
+      schoolPoint: 300,
+   },
+   {
+      name: 'Vanya',
+      surname: 'Pupikov',
+      ratingPoint: 700,
+      schoolPoint: 500,
+   },
+   {
+      name: 'Semen',
+      surname: 'Nosov',
+      ratingPoint: 620,
+      schoolPoint: 400,
+   },
+   {
+      name: 'Vitaliy',
+      surname: 'Komov',
+      ratingPoint: 980,
+      schoolPoint: 370,
+   },
+   {
+      name: 'Viktor',
+      surname: 'Zdanov',
+      ratingPoint: 1000,
+      schoolPoint: 480,
+   },
+   {
+      name: 'Svetlana',
+      surname: 'Senova',
+      ratingPoint: 820,
+      schoolPoint: 230,
+   },
+   {
+      name: 'Ludmila',
+      surname: 'Fedina',
+      ratingPoint: 890,
+      schoolPoint: 350,
+   },
+   {
+      name: 'Maksim',
+      surname: 'Zhuk',
+      ratingPoint: 630,
+      schoolPoint: 250,
+   },
+   {
+      name: 'Oleg',
+      surname: 'Salin',
+      ratingPoint: 720,
+      schoolPoint: 470,
+   },
+   {
+      name: 'Mihail',
+      surname: 'Krutikoiv',
+      ratingPoint: 820,
+      schoolPoint: 460,
+   }
+]; 
--- JavaScript/HW4/task 1/js/script.js
+++ JavaScript/HW4/task 1/js/script.js
@@ -1,44 +1,94 @@
 let id = 1;
-let studentsArrDone = [];
-let enrollee;
-
-let qwe = {};
-let asd = [];
+let student = {};
+let studArr = [];
+let candidate = [];
 
 
 class Student {
-   constructor(studentsArr, id, arr){
-      this.id = id;
-      this.name = studentsArr.name;
-      this.surname = studentsArr.surname;
-      this.ratingPoint = studentsArr.ratingPoint;
-      this.schoolPoint = studentsArr.schoolPoint;
+   constructor(enrollee){
+      this.id = id++;
+      this.name = enrollee.name;
+      this.surname = enrollee.surname;
+      this.ratingPoint = enrollee.ratingPoint;
+      this.schoolPoint = enrollee.schoolPoint;
+      this.isSelfPayment = true;
    }
 
-   static listOfStudens = function () {
-      return 1;
+   static listOfStudens() {
+      return studArr;
    }
 }
 
-let createEnrolleeArr = (studentsArr) => {
-   let enrolleeArr = [];
-   for (let stud of studentsArr) {
-      enrollee = new Student (stud, id++, asd);
-      enrolleeArr.push(enrollee);
-   }
-   return enrolleeArr;
-}
 
-new Student({ 
+
+let chelik = new Student({ 
    name: 'Valeriy',
    surname: 'Golovanov',
-   ratingPoint: 500,
-   schoolPoint: 200,
+   ratingPoint: 820,
+   schoolPoint: 310,
 });
+studArr.push(chelik)
 
-console.log(createEnrolleeArr(studentsArr, id, asd));
-console.log(Student.listOfStudents);
-console.log(asd)
+
+
+for (let stud of enrollee) {
+   student = new Student (stud);
+   studArr.push(student);
+   if (stud.ratingPoint >=800) {
+      candidate.push(student);
+   }
+}
+
+
+candidate.sort(function(a, b) {return b.ratingPoint - a.ratingPoint})
+
+let selfPaid = (arr) => {
+   let lastChanse = [candidate[4]];
+   let peoplesChanse = 1;
+   let notPaidId = [];
+   for (let i=4; i>=0; i--) {
+      if (candidate[i].ratingPoint !== candidate[i-1].ratingPoint) {
+         break;
+      }
+      lastChanse.unshift(candidate[i-1]);
+      peoplesChanse++
+   }
+   for (let i=4; i<candidate.length; i++) {
+      if (candidate[i].ratingPoint !== candidate[i+1].ratingPoint) {
+         break;
+      }
+      lastChanse.push(candidate[i+1]);
+   }
+   
+   lastChanse.sort(function(a, b) {return b.schoolPoint - a.schoolPoint})
+
+   for (let i=0; i<=4-peoplesChanse; i++) {
+      notPaidId.push(candidate[i].id)
+   }
+   for (let i=0; i<=peoplesChanse-1; i++) {
+      notPaidId.push(lastChanse[i].id)
+   }
+
+   for (let key of arr) {
+      key.isSelfPayment = true;
+      for (let num of notPaidId) {
+         if (key.id === num){
+            key.isSelfPayment = false;
+            break;
+         }
+      }
+   }
+
+   console.log('not paid: ',notPaidId);
+   return lastChanse;
+}
+console.log(selfPaid(studArr));
+
+
+
+console.log('candidate: ',candidate);
+console.log(Student.listOfStudens());
+
 
 
 
