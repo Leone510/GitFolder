d415288b095684b2b5c567a9f5c177c60e80a202
Leone510 <meteorologsio@gmail.com>
2020-12-27 14:08:43 +0200

    Task 7.2

--- JavaScript/HW7/js/script.js
+++ JavaScript/HW7/js/script.js
@@ -5,9 +5,17 @@ const searchCandidatesByPhoneNumber = phone => {
    return condidateArr.filter(condidate => condidate.phone.includes(phone))
 }
 
-//----------------------------- Task 1 ---------------------------------
+//----------------------------- Task 2 ---------------------------------
 
+const getCandidateById = id => {
+   let fixedDateCondidate = condidateArr.find(condidate => condidate._id === id)
+   
+   let fixDate = fixedDateCondidate.registered.split("").splice(0, 10).join('').split('-').reverse().join('-')
+   fixedDateCondidate.registered = fixDate;
+   return fixedDateCondidate;
+}
 
+//----------------------------- Task 3 ---------------------------------
 
 
 
